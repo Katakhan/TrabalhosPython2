a6b580bbabd06f4344b0f44a51c82da319f0a798
Tonhao <antoniorafaelgastaldi@hotmail.com>
2020-02-20 11:49:30 -0300

    aula 60 - 2

--- Aula59/A59C1.py
+++ Aula59/A59C1.py
@@ -1,12 +1,12 @@
-#---- Rev Classe
-#---- Métodos de Classe
-#---- Método __init__
-#---- Variáveis de classe
-#---- Variáveis privadas
-#---- Metodos Getters e Setters
+# ---- Rev Classe
+# ---- Métodos de Classe
+# ---- Método __init__
+# ---- Variáveis de classe
+# ---- Variáveis privadas
+# ---- Metodos Getters e Setters
 
 class Calc:
-    def __init__(self, numero1,numero2):
+    def __init__(self, numero1, numero2):
         # Variável de classe
         self.__n1 = numero1
         self.__n2 = numero2
@@ -14,11 +14,13 @@ class Calc:
 
     def set_n1(self, valor):
         self.__n1 = valor
+
     def get_n1(self):
         return self.__n1
 
     def set_n2(self, valor):
         self.__n2 = valor
+
     def get_n2(self):
         return self.__n2
 
@@ -37,7 +39,8 @@ class Calc:
         self.__resultado = self.__n1 - self.__n2
         return self.__resultado
 
-c = Calc(10,20)
+
+c = Calc(10, 20)
 assert isinstance(c, Calc)
 for i in range(1000):
     c.set_n1(i)
@@ -54,11 +57,11 @@ assert c.subtracao() == c.get_resultado()
 assert c.multiplicacao() == c.get_resultado()
 
 # Instanciando um objeto da classe Calc
-#c = Calc(10,20)
-#print(c.get_n1())
-#print(c.get_n2())
-#c.set_n1(50)
-#c.set_n2(100)
-#print(c.get_n1())
-#print(c.get_n2())
-#print(c.get_resultado())
+# c = Calc(10,20)
+# print(c.get_n1())
+# print(c.get_n2())
+# c.set_n1(50)
+# c.set_n2(100)
+# print(c.get_n1())
+# print(c.get_n2())
+# print(c.get_resultado())
